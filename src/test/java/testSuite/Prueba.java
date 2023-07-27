2c2b7a380d3d8ffc44dc564274e91922b3d21551
Seba Candia <sacandia01@gmail.com>
2023-07-27 14:03:25 -0400

    se agrega ubicacion chromedriver

--- src/test/java/testSuite/Prueba.java
+++ src/test/java/testSuite/Prueba.java
@@ -12,7 +12,7 @@ public class Prueba {
 
     @BeforeTest
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "");
+        System.setProperty("webdriver.chrome.driver", "driverNavegador/chromedriver.exe");
         webDriver = new ChromeDriver();
         webDriver.get(url);
     }
